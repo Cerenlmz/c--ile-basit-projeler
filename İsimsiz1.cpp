16d210ed8f4b59aaf129f9b9d6aa44c5cfb0dfe3
Cerenlmz <24180202031@gazi.edu.tr>
2025-02-16 15:09:13 +0300

    Add files via upload

---
+++ İsimsiz1.cpp
@@ -0,0 +1,23 @@
+//BASÝT BÝR ÇARPIM TABLOSU 
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a,b,c;
+	a=0;
+	b=0;
+	while(a<=10){
+	
+	for(b=0;b<=10;b++){
+			c=a*b;
+		printf("%d*%d=%d\n",a,b,c);
+	
+		
+	}
+	
+	a++;
+printf("\n");
+}
+	
+	return 0;
+}
