57d6c917380cc0841a67c3ee9f88baef7973cc90
Cerenlmz <24180202031@gazi.edu.tr>
2025-02-18 15:25:23 +0300

    Add files via upload

---
+++ c ile basit proje 3.cpp
@@ -0,0 +1,14 @@
+//GÝRÝLEN SAYININ FAKTORÝYELÝNÝ HESAPLAYAN PROGRAM
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int sayi,i=1,faktoriyel=1;
+	printf("Lutfen faktoriyel hesaplamak istediginiz sayiyi giriniz:\n");
+	scanf("%d",&sayi);
+	for(i=1;i<=sayi;i++){
+		faktoriyel=faktoriyel*i;
+	}
+	printf("%d!=%d\n",sayi,faktoriyel);
+	return 0;
+}
